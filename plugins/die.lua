d4533182e74cb8acaf2c683420dc332f8c6dec9a
arn <arn@reloaded.localdomain>
2017-06-30 18:44:35 +0530

    use os.exit(0)

--- plugins/die.lua
+++ plugins/die.lua
@@ -2,7 +2,7 @@ do
   local function run(msg, matches)
     sendText(msg.chat_id_, msg.id_, _msg("I'm gonna die 😫... RIP."), 0, '',
       function (a,d)
-        local output = util.shellCommand("pkill -f 'tg/telegram-cli -WRs ./bot/bot.lua -c '")
+        os.exit(0)
       end)
   end
 
